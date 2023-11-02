07b0373f3aa7eb4c38ccebe3eed4e7d4d4bcd437
Aniket Chavan <anikettchavan103@gmail.com>
2023-11-02 16:01:27 +0530

    Initial commit

---
+++ src/assets/data/services.js
@@ -0,0 +1,38 @@
+export const services = [
+  {
+    name: "Security Services",
+    desc: "We have the survilence cameras, and other majors to ensure the safety measures of the vehicles",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Online reservations",
+    desc: "Our online website helps to book your parking slot in advance for the time-saving and emergency purposes",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+  {
+    name: "Payment systems",
+    desc: "Our online web will help you to pay the charges as per your need of the time and will book your parking slot.",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "veicle parking:",
+    desc: "The areas which will we are allocating for the parking will be well partitioned as the the vehicle type and its consuming space",
+    bgColor: "rgba(1, 181, 197, .2)",
+    textColor: "#01B5C5",
+  },
+  {
+    name: "customer service",
+    desc: "If User have any query or doubt while the parking or at the time of booking that will also get solved.",
+    bgColor: "rgba(254, 182, 13, .2)",
+    textColor: "#FEB60D",
+  },
+  {
+    name: "Sustainability Initiatives",
+    desc: "Implementing eco-friendly practices, such as electric vehicle charging stations, and promoting carpooling or alternative transportation options",
+    bgColor: "rgba(151, 113, 255, .2)",
+    textColor: "#9771FF",
+  },
+];
---
+++ src/components/Testimonial/Testimonials.jsx
@@ -0,0 +1,110 @@
+   
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import patientAvatar from "../../assets/images/person1.png";
+import person2 from "../../assets/images/parson2.jpg";
+import p3 from "../../assets/images/person3.jpg";
+import { HiStar } from "react-icons/hi";
+
+const Testimonials = () => {
+  return (
+    <div className="mt-[30px] lg:mt-[55px]">
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={1}
+        breakpoints={{
+          640: {
+            slidesPerView: 1,
+            spaceBetween: 0,
+          },
+          768: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+          },
+          1024: {
+            slidesPerView: 3,
+            spaceBetween: 30,
+          },
+        }}
+      >
+        <SwiperSlide>
+          <div className="py-[30px] px-5 rounded-3">
+            <div className="flex items-center gap-[13px]">
+              <img src={patientAvatar} alt="" />
+              <div>
+                <h4 className="text-[18px] leading[30px] font-semibold text-headingColor">
+                  Sahil Chagule
+                </h4>
+                <div className="flex items-center gap-[2px]">
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                </div>
+              </div>
+            </div>
+            <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+            This parking system saved me time and stress, making parking hassle-free.,
+            </p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="py-[30px] px-5 rounded-3">
+            <div className="flex items-center gap-[13px]">
+              <img src={person2} alt="" />
+              <div>
+                <h4 className="text-[18px] leading[30px] font-semibold text-headingColor">
+                  Prasad Chaugale
+                </h4>
+                <div className="flex items-center gap-[2px]">
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                </div>
+              </div>
+            </div>
+            <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+            Parking has never been this easy and efficient. A must-try system!
+            </p>
+          </div>
+        </SwiperSlide>
+
+        <SwiperSlide>
+          <div className="py-[30px] px-5 rounded-3">
+            <div className="flex items-center gap-[13px]">
+              <img src={p3} alt="" />
+              <div>
+                <h4 className="text-[18px] leading[30px] font-semibold text-headingColor">
+                  Raj parit
+                </h4>
+                <div className="flex items-center gap-[2px]">
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                  <HiStar className="text-yellowColor w-[18px] h-5" />
+                </div>
+              </div>
+            </div>
+            <p className="text-[16px] leading-7 mt-4 text-textColor font-[400]">
+            The convenience of booking parking slots with this system is unparalleled
+            </p>
+          </div>
+        </SwiperSlide>
+
+     
+
+       
+        {/* Additional SwiperSlides here... */}
+      </Swiper>
+    </div>
+  );
+};
+
+export default Testimonials;
